580ff1cee190204b002f1f789088bb8fcdf88d99
johnnyvhp <jmerbernardo@gmail.com>
2025-04-25 15:59:22 +0000

    Added the analytics ready tables

---
+++ models/mart/fct_unique_men_products.sql
@@ -0,0 +1,22 @@
+WITH ranked_products AS (
+    SELECT *,
+        ROW_NUMBER() OVER (
+            PARTITION BY product_id
+            ORDER BY rrp DESC NULLS LAST, current_price DESC NULLS LAST
+        ) AS row_num
+    FROM {{ ref('stg_men_fashion') }}
+)
+
+SELECT
+    product_id,
+    brand_name,
+    title,
+    current_price,
+    previous_price,
+    colour,
+    currency,
+    rrp,
+    product_code,
+    product_type
+FROM ranked_products
+WHERE row_num = 1
